ad31c40314c776c4ffcfc3d152afecfc6decb650
Luca <lucatonello097@gmail.com>
2024-09-09 12:52:54 -0300

    add files and folders inside folders & improved UI for uploading

--- routes/route.js
+++ routes/route.js
@@ -9,7 +9,16 @@ const path = require("path");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        const folderName = req.params.folder;
+        const folderPath = path.join(__dirname, '..', 'uploads', req.params[0] || '');
+
+        fs.mkdir(folderPath, { recursive: true }, (err) => {
+            if (err) {
+                console.error('Error creating folder:', err);
+                return cb(err, folderPath);
+            }
+            cb(null, folderPath);
+        });
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname);
@@ -40,7 +49,7 @@ router.get('/login', (req, res) => {
     res.render("login");
 });
 router.post('/login', passport.authenticate('local', {
-    successRedirect: '/home',
+    successRedirect: '/files',
     failureRedirect: '/login'
 }))
 
@@ -48,22 +57,24 @@ router.get('/logout', (req, res) => {
     req.logout((err) => {
         if (err) {
             console.log('error loging out', err);
-            return res.redirect('/home');
+            return res.redirect('/files');
         }
         res.redirect("/");
     });
   });
 
-router.get('/home', (req, res) => {
-    res.render('home');
-});
+// router.get('/home', (req, res) => {
+//     res.render('home');
+// });
 
-router.post('/upload', upload.single('file'), async function (req, res, next) {
+router.post('/upload/*', upload.single('file'), async function (req, res, next) {
     if (req.file) {
         try {
             const { filename, path: filepath, size} = req.file;
+            const folderPath = req.params[0] || '';
+
             await pool.query('INSERT INTO FILES (filename, filepath, size) VALUES ($1, $2, $3)', [filename, filepath, size]);
-            res.send('File uploaded successfully.');
+            res.redirect(`/files/${encodeURIComponent(folderPath)}`);
         } catch (err) {
             console.error(err);
             res.status(500).send("error saving file");
@@ -73,23 +84,53 @@ router.post('/upload', upload.single('file'), async function (req, res, next) {
     }
 });
 
-router.post('/newfolder', (req, res) => {
-    const folderName = req.body.folderName;
-    const folderPath = path.join(__dirname, '..', 'uploads', folderName);
-    
-    fs.mkdir(folderPath, { recursive: true }, (err) => {
-        if (err) {
-            console.err(err);
-            return res.status(500).send("Error creating folder");
-        }
-        res.send("Foder created succesfully");
-    });
+router.post('/upload/files/:folder', upload.single('file'), async function (req, res, next) {
+    const folderName = req.params.folder;
+    if (req.file) {
+        try {
+            const { filename, path: filepath, size} = req.file;
+            const folderPath = path.join(__dirname, '..', 'uploads', folderName, filename);
+
+            await pool.query('INSERT INTO FILES (filename, filepath, size) VALUES ($1, $2, $3)', [filename, filepath, size]);
+            
+            res.redirect(`/files/${encodeURIComponent(folderName)}`);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send("error saving file");
+        }        
+    } else {
+        res.status(400).send('No file uploaded.');
+    }
 });
 
-router.get('/files', (req, res) => {
-    const uploadsPath = path.join(__dirname, '..', 'uploads');
+router.post('/newfolder/:parentFolder', async (req, res) => {
+    const folderName = req.body.folderName;
+    const parentFolder = req.params.parentFolder || '';
+    const folderPath = path.join(__dirname, '..', 'uploads', parentFolder, folderName);
 
-    fs.readdir(uploadsPath, { withFileTypes: true }, (err, files) => {
+    try {
+        await pool.query('INSERT INTO folders (foldername, folderpath) VALUES ($1, $2)', [folderName, folderPath]);
+
+        fs.mkdir(folderPath, { recursive: true }, (err) => {
+            if (err) {
+                console.err(err);
+                return res.status(500).send("Error creating folder");
+            }
+            res.redirect(`/files/${encodeURIComponent(parentFolder)}`);
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error adding folder to the database");
+    }
+
+    
+});
+
+router.get('/files/*', (req, res) => {
+    const folderPath = path.join(__dirname, '..', 'uploads', req.params[0]);    
+    const pageUrl = req.originalUrl;
+
+    fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
         if (err) {
             console.error(err);
             return res.status(500).send("error reading files");
@@ -97,15 +138,17 @@ router.get('/files', (req, res) => {
         const filesWithUrls = files.map(file => ({
             name: file.name,
             isDirectory: file.isDirectory(),
-            url: file.isDirectory() ? file.name : encodeURIComponent(file.name)
+            url: path.join(req.params[0], file.name)
         }));
-        res.render('files', { files: filesWithUrls, currentPath: '/' });
+        res.render('files', { files: filesWithUrls, currentPath: path.join('/', req.params[0]), pageUrl: pageUrl });
     });
 });
 
 router.get('/files/:folder', (req, res) => {
     const folderName = req.params.folder;
     const folderPath = path.join(__dirname, '..', 'uploads', folderName);
+    const pageUrl = req.originalUrl;
+
 
     fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
         if (err) {
@@ -117,7 +160,7 @@ router.get('/files/:folder', (req, res) => {
             isDirectory: file.isDirectory(),
             url: path.join(req.params.folder, file.name) // Construct path for folder
         }));
-        res.render('files', { files: filesWithUrls, currentPath: path.join('/', req.params.folder) });    
+        res.render('files', { files: filesWithUrls, currentPath: path.join('/', req.params.folder), pageUrl: pageUrl });    
     });
 });
 module.exports = router;
